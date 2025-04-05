14d27b263f800bd8787e9d766b9dfdf5f65e2e50
Euan Macinnes <euan.macinnes@pointscape.com.sg>
2025-04-05 10:16:48 +0800

    ok sorted out the series formats with colour and fill colour, though the visuals don't update properly the loading and saving does.

--- static/chart_options.js
+++ static/chart_options.js
@@ -1,9 +1,9 @@
 function chart_options_initialize(loop_index) {
-    const addSGBtn = document.getElementById("addSG"+loop_index);
-    const seriesGrp = document.getElementById("seriesGrp"+loop_index);
+    const addSGBtn = document.getElementById("addSG" + loop_index);
+    const seriesGrp = document.getElementById("seriesGrp" + loop_index);
 
-    const addSFBtn = document.getElementById("addSF"+loop_index);
-    const seriesFormat = document.getElementById("seriesFormat"+loop_index);
+    const addSFBtn = document.getElementById("addSF" + loop_index);
+    const seriesFormat = document.getElementById("seriesFormat" + loop_index);
 
     // ==== DRAG N DROP ====
     $(document).ready(function () {
@@ -101,14 +101,14 @@ function addSeriesGrp() {
     div.appendChild(select);
 
     const labels = [
-        { text: "X Axis", for: "x", class: "series-x", name: "x", value: "Batch" },
-        { text: "Y Axis", for: "y", class: "series-y", name: "y", value: "" },
-        { text: "Z Axis", for: "z", class: "series-z", name: "z", value: "" },
-        { text: "Y Min Axis", for: "ymin", class: "series-ymin", name: "ymin", value: "" },
-        { text: "Y Max Axis", for: "ymax", class: "series-ymax", name: "ymax", value: "" },
-        { text: "Series Axis", for: "s", class: "series-s", name: "s", value: "" },
-        { text: "Unit Axis", for: "u", class: "series-u", name: "u", value: "" },
-        { text: "Label Axis", for: "l", class: "series-l", name: "l", value: "" }
+        {text: "X Axis", for: "x", class: "series-x", name: "x", value: "Batch"},
+        {text: "Y Axis", for: "y", class: "series-y", name: "y", value: ""},
+        {text: "Z Axis", for: "z", class: "series-z", name: "z", value: ""},
+        {text: "Y Min Axis", for: "ymin", class: "series-ymin", name: "ymin", value: ""},
+        {text: "Y Max Axis", for: "ymax", class: "series-ymax", name: "ymax", value: ""},
+        {text: "Series Axis", for: "s", class: "series-s", name: "s", value: ""},
+        {text: "Unit Axis", for: "u", class: "series-u", name: "u", value: ""},
+        {text: "Label Axis", for: "l", class: "series-l", name: "l", value: ""}
     ];
 
     // eg,
@@ -153,12 +153,32 @@ function addSeriesFormat() {
     newSF.appendChild(div);
 
     const labels = [
-        { text: "Series Name / Regex", for: `nrx${newIndex}`, class: "format-nrx", name: "x", type: "string", value: "" },
-        { text: "Filled", for: `f${newIndex}`, class: "format-f", name: "f", type: "checkbox" },
-        { text: "Stepped", for: `p${newIndex}`, class: "format-p", name: "p", type: "checkbox" },
-        { text: "Dotted", for: `dt${newIndex}`, class: "format-dt", name: "dt", type: "checkbox" },
-        { text: "Point Size", for: `pr${newIndex}`, class: "format-pr", name: "pr", type: "number", value: "", step: "1", min: "0", max: "100" },
-        { text: "Line Width", for: `lw${newIndex}`, class: "format-lw", name: "lw", type: "number", value: "", step: "1", min: "0", max: "100" }
+        {text: "Series Name / Regex", for: `nrx${newIndex}`, class: "format-nrx", name: "x", type: "string", value: ""},
+        {text: "Filled", for: `f${newIndex}`, class: "format-f", name: "f", type: "checkbox"},
+        {text: "Stepped", for: `p${newIndex}`, class: "format-p", name: "p", type: "checkbox"},
+        {text: "Dotted", for: `dt${newIndex}`, class: "format-dt", name: "dt", type: "checkbox"},
+        {
+            text: "Point Size",
+            for: `pr${newIndex}`,
+            class: "format-pr",
+            name: "pr",
+            type: "number",
+            value: "",
+            step: "1",
+            min: "0",
+            max: "100"
+        },
+        {
+            text: "Line Width",
+            for: `lw${newIndex}`,
+            class: "format-lw",
+            name: "lw",
+            type: "number",
+            value: "",
+            step: "1",
+            min: "0",
+            max: "100"
+        }
     ];
     labels.forEach(labelInfo => {
         const label = document.createElement("label");
@@ -189,34 +209,57 @@ function addSeriesFormat() {
     const clabel = document.createElement("label");
     clabel.setAttribute("for", `col${newIndex}`);
     clabel.className = "form-label";
-    clabel.textContent = "Colour (named or hex)";
+    clabel.textContent = "Colour";
+
+    const clabel2 = document.createElement("label");
+    clabel2.setAttribute("for", `col-back${newIndex}`);
+    clabel2.className = "form-label";
+    clabel2.textContent = "Fill Colour";
 
     const divColorPicker = document.createElement("div");
     divColorPicker.className = "color-picker-container d-flex align-items-center";
+
+    const divColorPicker2 = document.createElement("div");
+    divColorPicker2.className = "color-picker-container d-flex align-items-center";
 
     const select = document.createElement("select");
     select.className = "form-control format-col";
     select.id = `col${newIndex}`;
     select.name = "color";
 
-    const options = [{ text: "Red", value: "rgb(255, 99, 132)" },
-    { text: "Orange", value: "rgb(255, 159, 64)" },
-    { text: "Yellow", value: "rgb(255, 205, 86)" },
-    { text: "Green", value: "rgb(75, 192, 192)" },
-    { text: "Blue", value: "rgb(54, 162, 235)" },
-    { text: "Purple", value: "rgb(153, 102, 255)" },
-    { text: "Grey", value: "rgb(201, 203, 207)" },
-    { text: "Custom Colour", value: "custom" }
+    const select2 = document.createElement("select");
+    select2.className = "form-control format-col-back";
+    select2.id = `col-back${newIndex}`;
+    select2.name = "color";
+
+    const options = [
+        {text: "Default", value: ""},
+        {text: "Red", value: "rgb(255, 99, 132)"},
+        {text: "Orange", value: "rgb(255, 159, 64)"},
+        {text: "Yellow", value: "rgb(255, 205, 86)"},
+        {text: "Green", value: "rgb(75, 192, 192)"},
+        {text: "Blue", value: "rgb(54, 162, 235)"},
+        {text: "Purple", value: "rgb(153, 102, 255)"},
+        {text: "Grey", value: "rgb(201, 203, 207)"},
+        {text: "Custom Colour", value: "custom"}
     ];
     options.forEach(optionInfo => {
         const option = document.createElement("option");
         option.textContent = optionInfo.text;
         option.value = optionInfo.value;
         select.appendChild(option);
+
+        const option2 = document.createElement("option");
+        option2.textContent = optionInfo.text;
+        option2.value = optionInfo.value;
+        select2.appendChild(option2);
     });
 
     const ellipsis = document.createElement("i");
     ellipsis.className = "bi bi-three-dots-vertical ellipsis";
+
+    const ellipsis2 = document.createElement("i");
+    ellipsis2.className = "bi bi-three-dots-vertical ellipsis";
 
     const colorPicker = document.createElement("input");
     colorPicker.type = "color";
@@ -224,17 +267,29 @@ function addSeriesFormat() {
     colorPicker.id = `col${newIndex}`;
     colorPicker.name = "cc";
 
+    const colorPicker2 = document.createElement("input");
+    colorPicker2.type = "color";
+    colorPicker2.className = "form-control format-col-picker";
+    colorPicker2.id = `col-back${newIndex}`;
+    colorPicker2.name = "cc2";
+
     div.appendChild(clabel);
     div.appendChild(divColorPicker);
+    div.appendChild(clabel2);
+    div.appendChild(divColorPicker2);
     divColorPicker.appendChild(select);
     divColorPicker.appendChild(ellipsis);
     divColorPicker.appendChild(colorPicker);
+
+    divColorPicker2.appendChild(select2);
+    divColorPicker2.appendChild(ellipsis2);
+    divColorPicker2.appendChild(colorPicker2);
 
     // Create a remove button to delete the row
     const removeBtn = document.createElement("i");
     removeBtn.className = "bi bi-trash ps-3 cell-delete-series-format data-cell-icon-hover remove";
     div.appendChild(removeBtn);
-    
+
     return newSF;
 }
 
