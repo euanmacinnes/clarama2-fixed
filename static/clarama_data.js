34f0703710a5c2efaa4120a322db4497ca176b22
Euan Macinnes <euan.macinnes@pointscape.com.sg>
2025-05-09 23:12:24 +0800

    Docstrings

--- static/clarama_data.js
+++ static/clarama_data.js
@@ -1,3 +1,16 @@
+/**
+ * Clarama Data JS - Functions for data visualization and manipulation
+ * @fileoverview This file provides functions for creating and manipulating data
+ * visualizations, including tables and charts, in the Clarama interface.
+ */
+
+/**
+ * Creates a Bootstrap table from provided data
+ * @param {string} table_id - ID of the HTML element where the table will be rendered
+ * @param {Object} table_data - Data object containing columns and rows
+ * @description Transforms the provided data into a format suitable for Bootstrap Table
+ * and initializes the table with appropriate options
+ */
 function bTable(table_id, table_data) {
     var table_columns = [];
     var table_rows = [];
